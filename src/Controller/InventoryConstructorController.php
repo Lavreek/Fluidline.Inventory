3e88dab30f903ac859747dba59c24932e74a6530
lavreek <lavreeksolacki@yandex.ru>
2023-08-30 12:48:49 +0300

    Settings updated

--- src/Controller/InventoryConstructorController.php
+++ src/Controller/InventoryConstructorController.php
@@ -4,14 +4,22 @@ namespace App\Controller;
 
 use App\Form\InventoryInputType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class InventoryConstructorController extends AbstractController
 {
-    #[Route('/inventory/constructor', name: 'app_inventory_constructor')]
-    public function index(): Response
+    #[Route('/inventory/constructor', name: 'app_inventory_constructor', methods: ['GET'])]
+    public function index(Request $request): Response|JsonResponse
     {
+        $remoteAddress = $request->server->get('REMOTE_ADDR');
+
+        if (!in_array($remoteAddress, ['77.50.146.14', '127.0.0.1'])) {
+            return new JsonResponse();
+        }
+
         $inventory_form = $this->createForm(InventoryInputType::class);
 
         return $this->render('inventory_constructor/index.html.twig', [
