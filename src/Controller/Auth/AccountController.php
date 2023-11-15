efa191d80b7a90d617eb4ac44557ddbbd48ec624
lavreek <lavreeksolacki@yandex.ru>
2023-11-15 17:33:01 +0300

    CSRF updated

---
+++ src/Controller/Auth/AccountController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Auth;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccountController extends AbstractController
+{
+    #[Route('/auth/account', name: 'auth_account')]
+    public function index(): Response
+    {
+        return $this->render('auth/account/index.html.twig', [
+            'controller_name' => 'AccountController',
+        ]);
+    }
+}
